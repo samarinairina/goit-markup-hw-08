4c79aa17ce1ce9a214e494917845aa630ae3cd89
irina <samarinairina2017@gmail.com>
2020-09-01 04:39:02 +0300

    внесены изменения

--- js/modal.js
+++ js/modal.js
@@ -4,11 +4,11 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
+  document.body.classList.toggle('modal-open');
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
-})().
+})();
